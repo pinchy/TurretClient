e1f445188f0826ba454b6da83fc180b7ceb60d44
pinchy <sebastien@bluechilli.com>
2016-01-26 11:37:08 +1100

    Initial commit

---
+++ Turret.cpp
@@ -0,0 +1,232 @@
+#include "Arduino.h"
+#include "Turret.h"
+
+Turret::Turret()
+{
+	pinMode(AZIMUTH_DIR_PIN, OUTPUT);
+	pinMode(AZIMUTH_STEP_PIN, OUTPUT);
+	pinMode(ELEVATION_DIR_PIN, OUTPUT);
+	pinMode(ELEVATION_STEP_PIN, OUTPUT);
+
+	pinMode(AZIMUTH_CUTOUT_PIN, INPUT);
+	pinMode(ELEVATION_CUTOUT_PIN, INPUT);
+
+	pinMode(FIRE_PIN, OUTPUT);
+	pinMode(MOTOR_POWER_PIN, OUTPUT);
+
+	digitalWrite(FIRE_PIN, LOW);
+	digitalWrite(MOTOR_POWER_PIN, HIGH);
+
+	pos = {0, 0};
+	_pos = {0, 0};
+
+	_motorPowerState = true;
+
+  	delay(500);
+}
+
+
+void Turret::goHome(void)
+{
+	motors(ON);
+	reset();
+
+	_elevate(ELEVATION_HOME);
+	_rotate(AZIMUTH_HOME);
+
+	motors(OFF);  
+}
+
+
+void Turret::reset(void)
+{
+	motors(ON);
+
+	while(!_isElevationCutout()) 
+		_elevate(1, true);
+
+	while(!_isAzimuthCutout())
+		_rotate(1, true);
+
+	pos = {0, 0};
+	_pos = {0, 0};
+}
+
+
+bool Turret::aim(void)
+{
+	return _rotate(pos.azimuth - _pos.azimuth) && _elevate(pos.elevation - _pos.elevation);
+}
+
+
+bool Turret::aim(int a, int e) 
+{
+	setTarget(a, e);
+	return aim();	
+}
+
+
+bool Turret::setTarget(int a, int e)
+{
+	if(_checkElevationTarget(e) && _checkAzimuthTarget(a))
+	{
+		pos.elevation = e;
+		pos.azimuth = a;
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+void Turret::aimAndFire(int a, int e)
+{
+	aim(a, e);
+	delay(500);
+	fire();
+}
+
+
+void Turret::fire(void)
+{
+	digitalWrite(FIRE_PIN, HIGH);
+	delay(250);
+	digitalWrite(FIRE_PIN, LOW);
+}
+
+
+bool Turret::_elevate(int delta)
+{
+	return _elevate(delta, false);
+}
+
+
+bool Turret::_elevate(int delta, bool overrideCutoutProtection)
+{
+	_setElevationDirection(delta);
+
+	if(!overrideCutoutProtection && _checkElevationTarget(delta))
+	{
+		return false;
+	}
+
+	for(int k = 0; k < abs(delta); k++)
+	{
+		digitalWrite(ELEVATION_STEP_PIN, LOW);
+		delayMicroseconds(2);
+		digitalWrite(ELEVATION_STEP_PIN, HIGH);
+		delayMicroseconds(ELEVATION_MOTOR_DELAY); 
+	}
+
+	_pos.elevation += delta;
+	return true;
+}
+
+
+bool Turret::_checkElevationTarget(int delta)
+{
+	return (_pos.elevation + delta > EL_MAX || _pos.elevation + delta < EL_MIN);
+}
+
+
+bool Turret::_isElevationCutout(void)
+{
+    return digitalRead(ELEVATION_CUTOUT_PIN) == LOW;
+}
+
+
+void Turret::_setElevationDirection(int delta)
+{
+	if(delta < 0)
+	{
+		digitalWrite(ELEVATION_DIR_PIN, LOW);
+	}
+	else
+	{
+		digitalWrite(ELEVATION_DIR_PIN, HIGH);
+	}
+	delay(1);
+}
+
+
+bool Turret::_rotate(int delta)
+{
+	return _rotate(delta, false);
+}
+
+
+bool Turret::_rotate(int delta, bool overrideCutoutProtection)
+{
+	_setAzimuthDirection(delta);
+
+	if(!overrideCutoutProtection && _checkAzimuthTarget(delta))
+	{
+		return false;
+	}
+
+	for(int k = 0; k < abs(delta); k++)
+	{
+		digitalWrite(AZIMUTH_STEP_PIN, LOW);
+		delayMicroseconds(2);
+		digitalWrite(AZIMUTH_STEP_PIN,HIGH);
+		delayMicroseconds(AZIMUTH_MOTOR_DELAY); 
+	}
+
+	_pos.azimuth += delta;
+	return true;
+}
+
+bool Turret::_checkAzimuthTarget(int delta)
+{
+	return (_pos.elevation + delta > EL_MAX || _pos.elevation + delta < EL_MIN);
+}
+
+
+bool Turret::_isAzimuthCutout(void)
+{
+    return digitalRead(AZIMUTH_CUTOUT_PIN) == LOW;
+}
+
+
+void Turret::_setAzimuthDirection(int delta)
+{
+	if(delta < 0)
+	{
+		digitalWrite(AZIMUTH_DIR_PIN, LOW);
+	}
+	else
+	{
+		digitalWrite(AZIMUTH_DIR_PIN, HIGH);
+	}
+	delay(1);
+}
+
+
+void Turret::motors(bool mode)
+{
+	digitalWrite(MOTOR_POWER_PIN, mode);
+	_motorPowerState = mode;
+}
+
+
+String Turret::toString(void)
+{
+	String str = "Azimuth: ";
+	str.concat(_pos.azimuth);
+	str.concat(" Elevation: ");
+	str.concat(_pos.elevation);
+	str.concat(" Weapon: ");
+	str.concat(_motorPowerState);
+	return str;
+}
+
+
+Turret::~Turret(){} // Nothing to destruct
+
+
+
+
+
+
---
+++ Turret.h
@@ -0,0 +1,74 @@
+/* Turret Control 
+    Sebastien Eckersley-Maslin
+*/
+#ifndef Turret_h
+#define Turret_h
+
+    #include "Arduino.h"
+
+	/* Arduino shield PIN declarations */
+	#define AZIMUTH_DIR_PIN 4
+	#define AZIMUTH_STEP_PIN 5
+	#define ELEVATION_DIR_PIN 7
+	#define ELEVATION_STEP_PIN 6
+	#define AZIMUTH_CUTOUT_PIN 2
+	#define ELEVATION_CUTOUT_PIN 3 
+	#define FIRE_PIN 8
+	#define MOTOR_POWER_PIN 10
+
+	/* Motor controller STEP delay */
+	#define AZIMUTH_MOTOR_DELAY 800  //  800 good
+	#define ELEVATION_MOTOR_DELAY 1000  // 1000 good
+
+	/* Azimuth and Elevation home positions */
+	#define AZIMUTH_HOME 0  // -1983
+	#define ELEVATION_HOME -2030 // -2030
+	#define STEPS_PER_DEGREE 53.3333333
+
+	/* Safe to Fire Zone (STFZ) cutouts */
+	#define AZ_MAX 0
+	#define AZ_MIN -14000
+	#define EL_MAX 0
+	#define EL_MIN -5000
+
+	#define OFF LOW
+	#define ON HIGH
+
+	struct Position 
+	{
+		int elevation;
+		int azimuth;
+	};
+
+
+	class Turret
+	{
+	public:
+		Turret();
+		~Turret();
+		Position pos;
+		void goHome(void);
+		void reset(void);
+		bool aim(void);
+		bool aim(int a, int e);
+		bool setTarget(int a, int e);
+		void aimAndFire(int a, int e);
+		void fire(void);
+		void motors(bool mode);
+		String toString(void);
+	private:
+		Position _pos;
+		bool _motorPowerState;
+		bool _elevate(int delta);
+		bool _elevate(int delta, bool override);
+		bool _rotate(int delta);
+		bool _rotate(int delta, bool override);
+		bool _checkElevationTarget(int delta);
+		bool _isElevationCutout(void);
+		void _setElevationDirection(int delta);
+		bool _checkAzimuthTarget(int delta);
+		bool _isAzimuthCutout(void);
+		void _setAzimuthDirection(int delta);
+	};
+
+#endif
